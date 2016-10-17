d6f41cb819356db54db6e9bc6efed75bccd08f11
Андрей Кумыков <aakumykov@yandex.ru>
2016-10-17 10:44:00 +0700

    Компонент списка карточек

--- app/app.module.js
+++ app/app.module.js
@@ -1,3 +1,5 @@
 'use strict';
 
-angular.module('MainApp',[]);
+angular.module('MainApp',[
+	'CardList',
+]);
---
+++ app/card-list/card-list.component.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('CardList').
+	component('cardList',{
+		templateUrl: 'card-list/card-list.template.html',
+		controller: ['$http', function CardListController($http){
+			var self = this;
+			
+			$http.get('cards/list.json').then(function(response){
+				self.cardList = response.data;
+
+				for (var i in self.cardList) {
+					console.log(self.cardList[i]);
+				}
+			});
+		}]
+	}
+);
