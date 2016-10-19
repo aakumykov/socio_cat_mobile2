b7cdccf4164a93b67608960fb9956b44ebbe0f41
Андрей Кумыков <aakumykov@yandex.ru>
2016-10-19 10:33:16 +0700

    Боковая панель, ошибки

--- app/page-header/page-header.component.js
+++ app/page-header/page-header.component.js
@@ -3,7 +3,24 @@
 angular.module('PageHeader').
 	component('pageHeader',{
 		templateUrl: 'page-header/page-header.template.html',
-		controller: [ function(){
-			
+		controller: [ '$scope', function($scope){
+			$scope.showSideNav = function(){
+				document.getElementById("full_page").style.marginLeft = $scope.sideNavWidth;
+				document.getElementById("full_page").style.marginRight = '-'+$scope.sideNavWidth;
+				
+				document.getElementById("side_nav").style.width = $scope.sideNavWidth;
+				document.getElementById("side_nav").style.display = "block";
+				
+				document.getElementById("sidebar_button").style.display = 'none';
+			};
+			$scope.hideSideNav = function(){
+				document.getElementById("full_page").style.marginLeft = '0%';
+				document.getElementById("full_page").style.marginRight = '0%';
+				
+				document.getElementById("side_nav").style.width = $scope.sideNavWidth;
+				document.getElementById("side_nav").style.display = "none";
+
+				document.getElementById("sidebar_button").style.display = 'inline';
+			};
 		}]
 	});
---
+++ app/side-bar/side-bar.component.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('SideBar').
+	component('sideBar',{
+		templateUrl: 'side-bar/side-bar.template.html',
+		controller: [ function(){
+			
+		}]
+	});
