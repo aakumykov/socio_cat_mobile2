4c9d9992cc15226d66925b03833bad96bd4f454d
Андрей Кумыков <aakumykov@yandex.ru>
2016-10-24 15:39:05 +0700

    Что-то начало получаться

--- app/side-bar/side-bar.component.js
+++ app/side-bar/side-bar.component.js
@@ -1,7 +1,17 @@
 'use strict';
 
+function SideBarController(){
+	this.visible = true;
+
+	this.status = function(){
+		console.log('SideBarController.status()');
+		return this.visible;
+	}
+}
+
 var module = angular.module('SideBar')
 
 module.component('sideBar', {
 	templateUrl: 'side-bar/side-bar.template.html',
+	controller: SideBarController,
 });
