2b2f9b1927a3f8cc83eebe9d393210f9e5e328fa
Андрей Кумыков <aakumykov@yandex.ru>
2016-10-24 23:12:39 +0700

    Пока не идёт

--- app/side-bar/side-bar.component.js
+++ app/side-bar/side-bar.component.js
@@ -1,32 +1,43 @@
 'use strict';
 
+function SideBarController($rootScope, $scope){
+	this.visibe = true;
+	this.statusRequest = function(){
+		console.debug('SideBarController.statusRequest('+this.visibe+')');
+		return this.visibe;
+	}
+	this.showRequest = function(){
+		console.debug('SideBarController.showRequest()');
+		$rootScope.$broadcast('sidebar', {cmd:'show'});
+	}
+	this.hideRequest = function(){
+		console.debug('SideBarController.hideRequest()');
+		$rootScope.$broadcast('sidebar', {cmd:'hide'});
+	}
+	this.showResponse = function(){
+		console.debug('SideBarController.showResponse()');
+		var self = this;
+		$scope.$on('sidebar', function(cmd){
+			self.visibe = true;
+		});
+	}
+	this.hideResponse = function(){
+		console.debug('SideBarController.hideResponse()');
+		var self = this;
+		$scope.$on('sidebar', function(cmd){
+			self.visibe = false;
+		});
+	}
+}
+
 var module = angular.module('SideBar');
 
 module.component('sideBar',{
 	templateUrl: 'side-bar/side-bar.template.html',
-	controller: [ 'ss', function SideBarController(ss){
-		this.close = function(){
-			console.log('SideBarController.close()');
-			ss.hide();
-		}
-		this.status = function(){
-			console.log('SideBarController.status()');
-			return ss.status();
-		}
-	}]
+	controller: SideBarController,
 });
 
 module.component('sideBarToggler',{
-	templateUrl: 'side-bar/side-bar-toggler.template.html',
+	template: "<a ng-click='$ctrl.show()'' href='javascript:void(0)' id='sidebar_button' title='Меню'>@Меню</a>",
 	replace: true,
-	controller: [ 'ss', function SideBarController(ss){
-		this.close = function(){
-			console.log('SideBarController.close()');
-			ss.toggle();
-		}
-		this.status = function(){
-			console.log('SideBarController.status()');
-			return ss.status();
-		}
-	}]
 });
