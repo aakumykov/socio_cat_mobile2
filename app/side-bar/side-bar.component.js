d6d4782341f0638e5707708064f99a33eab71ec5
Андрей Кумыков <aakumykov@yandex.ru>
2016-10-24 15:43:11 +0700

    Что-то начало получаться 2

--- app/side-bar/side-bar.component.js
+++ app/side-bar/side-bar.component.js
@@ -7,6 +7,11 @@ function SideBarController(){
 		console.log('SideBarController.status()');
 		return this.visible;
 	}
+
+	this.hide = function(){
+		console.log('SideBarController.hide()');
+		this.visible = false;
+	}
 }
 
 var module = angular.module('SideBar')
