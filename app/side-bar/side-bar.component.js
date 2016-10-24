2fe3034d691322f746b92b7727acf3ba4fa89e6f
Андрей Кумыков <aakumykov@yandex.ru>
2016-10-24 12:49:43 +0700

    Служба для боковой панели

--- app/side-bar/side-bar.component.js
+++ app/side-bar/side-bar.component.js
@@ -2,6 +2,19 @@
 
 var module = angular.module('SideBar');
 
+module.service('sidebar', function(){
+	this.visible = true;
+
+	this.status = function(){
+		return this.visible;
+	}
+
+	this.toggle = function(){
+		this.visible = !this.visible;
+	}
+});
+
+
 module.component('sideBar',{
 	templateUrl: 'side-bar/side-bar.template.html',
 	bindings: {
@@ -10,9 +23,3 @@ module.component('sideBar',{
 	},
 });
 
-// module.component('sideBarToggler',{
-// 	templateUrl: 'side-bar/side-bar-toggler.template.html',
-// 	bindings: {
-// 		action: '&',
-// 	},
-// });
