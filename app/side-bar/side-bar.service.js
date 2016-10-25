b821f280d28e06b9370f5be009441f54601705b1
Андрей Кумыков <aakumykov@yandex.ru>
2016-10-25 11:06:48 +0700

    fix: return из службы; удалил старый код

--- app/side-bar/side-bar.service.js
+++ app/side-bar/side-bar.service.js
@@ -5,41 +5,30 @@ var module = angular.module('SideBar');
 module.factory('sideBarService', function(){
 	var visibility = true;
 
+	this.actions = {
+		toggle: function(){
+			console.debug('sideBarService.toggle()');
+			visibility = !visibility;
+		},
+		show: function(){
+			console.debug('sideBarService.show()');
+			visibility = true;
+		},
+		hide: function(){
+			console.debug('sideBarService.hide()');
+			visibility = false;
+		},
+		status: function(){
+			console.debug('sideBarService.status('+visibility+')');
+			return visibility;
+		},
+	}
+
+	var self = this;
+
 	var cmd = function(name){
 		console.debug('sideBarService.cmd('+name+')');
-
-		switch (name) {
-			case 'status':
-				return status();
-			case 'show':
-				return show();
-			case 'hide':
-				return hide();
-			case 'toggle':
-				return toggle();
-			default:
-				console.error("неизвестная команда '"+name+"'");
-		}
-	}
-
-	var toggle = function(){
-		console.debug('sideBarService.toggle()');
-		visibility = !visibility;
-	}
-
-	var show = function(){
-		console.debug('sideBarService.show()');
-		visibility = true;
-	}
-
-	var hide = function(){
-		console.debug('sideBarService.hide()');
-		visibility = false;
-	}
-
-	var status = function(){
-		console.debug('sideBarService.status('+visibility+')');
-		return visibility;
+		return self.actions[name]();
 	}
 
 	return {
