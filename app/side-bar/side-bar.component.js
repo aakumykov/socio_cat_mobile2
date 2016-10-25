aa9a836e9d4665a272f1fb65ef6d5e09fc717fd9
Андрей Кумыков <aakumykov@yandex.ru>
2016-10-25 10:13:22 +0700

    Запрос статуса работает

--- app/side-bar/side-bar.component.js
+++ app/side-bar/side-bar.component.js
@@ -1,7 +1,18 @@
 'use strict';
 
 function SideBarController(sideBarService) {
-
+	this.visibility = false;
+	var self = this;
+	
+	sideBarService.onToggle(function(visibility){
+		console.debug('SideBarController.onToggle.CALLBACK('+this.visibility+')');
+		self.visibility = visibility;
+	});
+	
+	this.status = function(){
+		console.debug('SideBarController.status('+this.visibility+')');
+		return this.visibility;
+	}
 }
 
 var module = angular.module('SideBar');
--- app/side-bar/side-bar.service.js
+++ app/side-bar/side-bar.service.js
@@ -8,15 +8,18 @@ module.factory('sideBarService', function(){
 	var all_callbacks = [];
 	
 	var onToggle = function(callback){
+		console.debug('sideBarService.onToggle()');
 		all_callbacks.push(callback);
 	}
 
 	var toggle = function(){
+		console.debug('sideBarService.toggle()');
 		visible = !visible;
 		for (i=0; i<all_callbacks.length; i++) {
 			all_callbacks[i](visible);
 		}
 	}
+	
 	return {
 		onToggle: onToggle,
 		toggle: toggle,
